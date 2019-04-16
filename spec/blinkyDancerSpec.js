cb24e1ea9907e225afcca22b4faf29d0692653a4
wchsinyi <hsinyi.wc.uma@gmail.com>
2019-04-15 18:27:05 -0700

    Complete refactor of Dancer and BlinkyDancer pseudoclassical class

--- spec/blinkyDancerSpec.js
+++ spec/blinkyDancerSpec.js
@@ -5,7 +5,7 @@ describe('blinkyDancer', function() {
 
   beforeEach(function() {
     clock = sinon.useFakeTimers();
-    blinkyDancer = makeBlinkyDancer(10, 20, timeBetweenSteps);
+    blinkyDancer = new makeBlinkyDancer(10, 20, timeBetweenSteps);
   });
 
   it('should have a jQuery $node object', function() {
@@ -13,6 +13,7 @@ describe('blinkyDancer', function() {
   });
 
   it('should have a step function that makes its node blink', function() {
+    // debugger;
     sinon.spy(blinkyDancer.$node, 'toggle');
     blinkyDancer.step();
     expect(blinkyDancer.$node.toggle.called).to.be.true;
